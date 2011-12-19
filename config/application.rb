84c321bd7039e6527c5af97541da249f99f2b161
byronaustin <byronaustin007@gmail.com>
2011-12-18 22:32:23 -0700

    fixed application.rb

--- config/application.rb
+++ config/application.rb
@@ -2,18 +2,16 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
-<<<<<<< HEAD
 # If you have a Gemfile, require the gems listed there, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(:default, Rails.env) if defined?(Bundler)
-=======
+
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
   Bundler.require(*Rails.groups(:assets => %w(development test)))
   # If you want your assets lazily compiled in production, use this line
   # Bundler.require(:default, :assets, Rails.env)
 end
->>>>>>> cb3d79854a2acfb096f41d8be9130f8cf7e413e1
 
 module DemoApp
   class Application < Rails::Application
@@ -39,25 +37,19 @@ module DemoApp
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
-<<<<<<< HEAD
     # JavaScript files you want as :defaults (application.js is always included).
     # config.action_view.javascript_expansions[:defaults] = %w(jquery rails)
 
-=======
->>>>>>> cb3d79854a2acfb096f41d8be9130f8cf7e413e1
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
-<<<<<<< HEAD
-=======
 
     # Enable the asset pipeline
     config.assets.enabled = true
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
->>>>>>> cb3d79854a2acfb096f41d8be9130f8cf7e413e1
   end
 end
